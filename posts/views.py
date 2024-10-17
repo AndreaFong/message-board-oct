b3fe0a8def925ca3c0ff0fb16821245388fc6e81
AndreaFong <Andrea@gmail.com>
2024-10-17 18:24:41 -0400

    post detail & new post link

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Publication
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView
 
 # Create your views here.
@@ -12,3 +12,7 @@ class PostCreateView(CreateView):
     model = Publication
     template_name = "post_new.html"
     fields = ["text",]
+
+class PostDetailView(DetailView):
+    model = Publication
+    template_name = "post_detail.html"
